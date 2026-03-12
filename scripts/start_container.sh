c2ea9f55d195288c017e721df612c852fe603e40
djitao <45945409+djitao@users.noreply.github.com>
2026-03-12 15:36:44 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo
-
+docker pull djitao/simple-python-flask-app
 # Run the Docker image as a container
 echo
+docker run -d -p 5000:5000 djitao/simple-python-flask-app
