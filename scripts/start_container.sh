3fcf9ea8e654e68beacf7546d684eea11951ddb8
djitao <45945409+djitao@users.noreply.github.com>
2026-04-14 19:01:23 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ echo
 docker pull djitao/simple-python-flask-app
 # Run the Docker image as a container
 echo
-docker run -d -p 5000:5000 djitao/simple-python-flask-app
+docker run -d -p 5000:5000 djitao/simple-python-flask-app 
