797ea9a95290de64662626987a17eac94c765a9a
djitao <45945409+djitao@users.noreply.github.com>
2026-04-20 13:34:00 +0200

    Update stop_container.sh

--- simple-python-app/stop_container.sh
+++ simple-python-app/stop_container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid=$(docker ps -q) 
-docker rm -f $containerid       
+containerid=$(docker ps -q)
+
+if [ -n "$containerid" ]; then
+  docker rm -f $containerid
+else
+  echo "No running containers to remove"
+fi
