0293c40679b6e3c4d7fe69502ad24a008c08fb66
djitao <45945409+djitao@users.noreply.github.com>
2026-04-21 10:25:01 +0200

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
 
-containers=$(docker ps -aq | xargs)
-
-if [ -z "$containers" ]; then
-  echo "No containers to remove"
-  exit 0
-fi
-
-docker rm -f $containers
+docker ps -aq | xargs -r docker rm -f
